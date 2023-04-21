2725aafaadf4b3d46561475743a82080da5a66c8
Janussr <Janusstivang@gmail.com>
2023-04-21 15:34:36 +0200

    bar chart needs a little tweaking

--- client/src/views/VisualizationFour.js
+++ client/src/views/VisualizationFour.js
@@ -1,84 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
+import apiUtils from '../utils/apiUtils';
+import { useState, useEffect } from "react"
 
-const data2012 = [
-    {quarter: 1, earnings: 13000},
-    {quarter: 2, earnings: 16500},
-    {quarter: 3, earnings: 14250},
-    {quarter: 4, earnings: 19000}
-  ];
   
-  const data2013 = [
-    {quarter: 1, earnings: 15000},
-    {quarter: 2, earnings: 12500},
-    {quarter: 3, earnings: 19500},
-    {quarter: 4, earnings: 13000}
-  ];
-  
-  const data2014 = [
-    {quarter: 1, earnings: 11500},
-    {quarter: 2, earnings: 13250},
-    {quarter: 3, earnings: 20000},
-    {quarter: 4, earnings: 15500}
-  ];
-  
-  const data2015 = [
-    {quarter: 1, earnings: 18000},
-    {quarter: 2, earnings: 13250},
-    {quarter: 3, earnings: 15000},
-    {quarter: 4, earnings: 12000}
-  ];
+const URL = apiUtils.getUrl()
   
 const VisualizationFour = () => {
+  const [companies, setCompanies] = useState([]);
 
-    return (
+  useEffect(() => {
+    const getCompanies = async () => {
+      const response = await apiUtils.getAxios().get(`${URL}/numbers`);
+      setCompanies(response.data);
+      console.log("response");
+    };
+    getCompanies();
+  }, [URL]);
 
-        <div className="center">
-            <h1>Four</h1>
+  const chartData = companies.map(({ name, employees }) => ({ x: name, y: employees }));
 
-            <VictoryChart
-        domainPadding={50}
-        theme={VictoryTheme.material}
-      >
-        <VictoryAxis
-          tickValues={[1, 2, 3, 4]}
-          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-        />
-        <VictoryAxis
-          dependentAxis
-          tickFormat={(x) => (`$${x / 1000}k`)}
-        />
-        <VictoryStack
-          colorScale={"warm"}
-        >
-          <VictoryBar
-            data={data2012}
-            x="quarter"
-            y="earnings"
-          />
-          <VictoryBar
-            data={data2013}
-            x="quarter"
-            y="earnings"
-          />
-          <VictoryBar
-            data={data2014}
-            x="quarter"
-            y="earnings"
-          />
-          <VictoryBar
-            data={data2015}
-            x="quarter"
-            y="earnings"
-          />
-        </VictoryStack>
+  return (
+    <div className="center">
+      <h1>Four</h1>
+      <VictoryChart domainPadding={25} theme={VictoryTheme.material}>
+      <VictoryAxis style={{tickLabels: {angle: 90, fontFamily: 'Arial', fontSize: 12 } }}/>
+        <VictoryAxis dependentAxis   />
+        <VictoryBar  
+       
+        data={chartData} />
       </VictoryChart>
+    </div>
+  );
+};
 
-        </div>
-    )
-
-}
 
 
 
